f8b6eef9908046cabb92befa6eb78637c39e3e69
Jorge Alcoforado <jorge.alcoforado@sam.fortaleza.ce.gov.br>
2020-02-19 16:31:11 -0300

    first commit

---
+++ src/componentes/PrimeiroComponete.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default (props) =>
+    <div>
+        <h1>{props.valor} </h1>
+    </div>   
+
+
+// function primeiro() {
+
+//     return <h1>Primeiro Componente!</h1>
+// }
+// export default primeiro
